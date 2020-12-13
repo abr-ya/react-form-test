b4667988af2ae868dbbec436b7e17d3782ac3b69
abr-ya <gamer05@list.ru>
2020-12-13 23:59:46 +0300

    Update CommentBox tests

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,9 +2,39 @@ import React from 'react';
 import {mount} from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
-it('has a text area and a button', () => {
-  const component = mount(<CommentBox />);
+let component;
 
+beforeEach(() => {
+  component = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  component.unmount();
+});
+
+it('has a text area and a button', () => {
   expect(component.find('textarea').length).toEqual(1);
   expect(component.find('button').length).toEqual(1);
+});
+
+describe('the text area', () => {
+  // использоуем в двух тестах - ввод теста и обновление
+  beforeEach(() => {
+    component.find('textarea').simulate('change', {
+      target: {value: 'test comment'},
+    });
+    component.update();
+  });
+
+  // проверяем значение после ввода
+  it('has a text area that user can type in', () => {
+    expect(component.find('textarea').prop('value')).toEqual('test comment');
+  });
+
+  // отправляем - обновляем - проверяем
+  it('has an empty textarea when form is submitted', () => {
+    component.find('form').simulate('submit');
+    component.update();
+    expect(component.find('textarea').prop('value')).toEqual('');
+  });
 });
